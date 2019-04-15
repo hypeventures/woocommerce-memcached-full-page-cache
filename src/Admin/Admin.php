06724bd8ac92de2c0d758beed93f8606f2d233ff
Achim Galeski <achim@invinciblebrands.com>
2019-04-15 17:14:56 +0200

    added empty Admin::class

---
+++ src/Admin/Admin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace InvincibleBrands\WcMfpc\Admin;
+
+
+if (! defined('ABSPATH')) { exit; }
+
+/**
+ * Class Admin
+ *
+ * @package InvincibleBrands\WcMfpc\Admin
+ */
+class Admin
+{
+
+}
