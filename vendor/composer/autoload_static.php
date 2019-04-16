d43140932c13fb3d0848ecf7b5b96478bbe02973
Achim Galeski <achim@invinciblebrands.com>
2019-04-16 18:50:58 +0200

    added Data::class to autoloader

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,6 +8,7 @@ class ComposerStaticInit9042dda03b2c5b78aa2a92d82f443d22
 {
     public static $classMap = array (
         'InvincibleBrands\\WcMfpc\\Admin\\Admin' => __DIR__ . '/../..' . '/src/Admin/Admin.php',
+        'InvincibleBrands\\WcMfpc\\Data' => __DIR__ . '/../..' . '/src/Data.php',
         'InvincibleBrands\\WcMfpc\\Memcached' => __DIR__ . '/../..' . '/src/Memcached.php',
         'InvincibleBrands\\WcMfpc\\WcMfpc' => __DIR__ . '/../..' . '/src/WcMfpc.php',
     );
