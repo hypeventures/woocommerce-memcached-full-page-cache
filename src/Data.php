d06c708f1bde6ffde908e774bf4cf81562f323bc
Achim Galeski <achim.galeski@gmail.com>
2019-04-27 21:02:29 +0200

    removed pre-cache & moved to admin.php

--- src/Data.php
+++ src/Data.php
@@ -15,20 +15,11 @@ class Data
 
     const host_separator         = ',';
     const port_separator         = ':';
-    const donation_id_key        = 'hosted_button_id=';
     const global_config_var      = '$wcMfpcConfig';
     const key_save               = 'saved';
     const key_delete             = 'deleted';
     const key_flush              = 'flushed';
     const slug_flush             = '&flushed=true';
-    const key_precache           = 'precached';
-    const slug_precache          = '&precached=true';
-    const key_precache_disabled  = 'precache_disabled';
-    const slug_precache_disabled = '&precache_disabled=true';
-    const precache_log           = 'wc-mfpc-precache-log';
-    const precache_timestamp     = 'wc-mfpc-precache-timestamp';
-    const precache_php           = 'wc-mfpc-precache.php';
-    const precache_id            = 'wc-mfpc-precache-task';
     const slug_save              = '&saved=true';
     const slug_delete            = '&deleted=true';
     const common_slug            = 'wp-common/';
@@ -36,7 +27,6 @@ class Data
     const plugin_name            = 'WC-MFPC';
     const capability             = 'manage_options';
     const global_option          = 'wc-mfpc-global';
-    const button_precache        = 'wc-mfpc-precache';
     const button_flush           = 'wc-mfpc-flush';
     const button_save            = 'wc-mfpc-save';
     const button_delete          = 'wc-mfpc-delete';
@@ -68,16 +58,6 @@ class Data
      * @var string
      */
     public $admin_css_url = '';
-
-    /**
-     * @var string
-     */
-    public $precache_logfile = '';
-
-    /**
-     * @var string
-     */
-    public $precache_phpfile = '';
 
     /**
      * @var mixed|string
@@ -126,13 +106,12 @@ class Data
         $this->plugin_url           = plugin_dir_url(dirname(__FILE__));
         $this->plugin_dir           = plugin_dir_path(dirname(__FILE__));
         $this->admin_css_url        = $this->plugin_url . 'assets/admin.css';
-        $this->precache_logfile     = sys_get_temp_dir() . '/' . self::precache_log;
-        $this->precache_phpfile     = sys_get_temp_dir() . '/' . self::precache_php;
         $this->acache_worker        = $this->plugin_dir . 'wc-mfpc-advanced-cache.php';
         $this->acache               = WP_CONTENT_DIR . '/advanced-cache.php';
+        $this->settings_slug        = 'admin.php';
+        $this->settings_link        = $this->settings_slug . '?page=' . Data::plugin_settings_page;
 
         $this->setShellFunction();
-        $this->setSettingsLinkAndSlug();
     }
 
     /**
@@ -154,27 +133,6 @@ class Data
             }
 
         }
-    }
-
-    /**
-     * Checks if plugin is activated via network and sets Slug & Link for the Settings-Page accordingly.
-     *
-     * @return void
-     */
-    private function setSettingsLinkAndSlug()
-    {
-        $this->settings_slug = 'options-general.php';
-
-        if (! function_exists('is_plugin_active_for_network') && @is_plugin_active_for_network($this->plugin_file)) {
-
-            require_once(ABSPATH . 'wp-admin/includes/plugin.php');
-
-            $this->network       = true;
-            $this->settings_slug = 'settings.php';
-
-        }
-
-        $this->settings_link = $this->settings_slug . '?page=' . Data::plugin_settings_page;
     }
 
 }
