3fc087ddca7ba4db199d8776ef68607167820b57
Achim Galeski <achim.galeski@gmail.com>
2019-04-27 22:05:09 +0200

    updated autoloader

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -7,10 +7,10 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'InvincibleBrands\\WcMfpc\\Admin' => $baseDir . '/src/Admin.php',
+    'InvincibleBrands\\WcMfpc\\AdminView' => $baseDir . '/src/AdminView.php',
     'InvincibleBrands\\WcMfpc\\Alert' => $baseDir . '/src/Alert.php',
     'InvincibleBrands\\WcMfpc\\Config' => $baseDir . '/src/Config.php',
     'InvincibleBrands\\WcMfpc\\Data' => $baseDir . '/src/Data.php',
-    'InvincibleBrands\\WcMfpc\\GlobalConfig' => $baseDir . '/src/GlobalConfig.php',
     'InvincibleBrands\\WcMfpc\\Memcached' => $baseDir . '/src/Memcached.php',
     'InvincibleBrands\\WcMfpc\\WcMfpc' => $baseDir . '/src/WcMfpc.php',
 );
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,10 +8,10 @@ class ComposerStaticInit9042dda03b2c5b78aa2a92d82f443d22
 {
     public static $classMap = array (
         'InvincibleBrands\\WcMfpc\\Admin' => __DIR__ . '/../..' . '/src/Admin.php',
+        'InvincibleBrands\\WcMfpc\\AdminView' => __DIR__ . '/../..' . '/src/AdminView.php',
         'InvincibleBrands\\WcMfpc\\Alert' => __DIR__ . '/../..' . '/src/Alert.php',
         'InvincibleBrands\\WcMfpc\\Config' => __DIR__ . '/../..' . '/src/Config.php',
         'InvincibleBrands\\WcMfpc\\Data' => __DIR__ . '/../..' . '/src/Data.php',
-        'InvincibleBrands\\WcMfpc\\GlobalConfig' => __DIR__ . '/../..' . '/src/GlobalConfig.php',
         'InvincibleBrands\\WcMfpc\\Memcached' => __DIR__ . '/../..' . '/src/Memcached.php',
         'InvincibleBrands\\WcMfpc\\WcMfpc' => __DIR__ . '/../..' . '/src/WcMfpc.php',
     );
