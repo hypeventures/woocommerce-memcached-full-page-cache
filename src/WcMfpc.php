ee60a63f6e68b236a2aa9e7bddd36b91ac54881e
Achim Galeski <achim.galeski@gmail.com>
2019-05-03 15:21:35 +0200

    added WcMfpc::getMemached()

--- src/WcMfpc.php
+++ src/WcMfpc.php
@@ -44,11 +44,6 @@ class WcMfpc
         }
 
         /*
-         * initiate memcached
-         */
-        $this->memcached = new Memcached($wcMfpcConfig->getConfig());
-
-        /*
          * if WP_CACHE is not set or false - abort here and safe your time.
          */
         if (! defined('WP_CACHE') || empty(WP_CACHE)) {
@@ -59,28 +54,28 @@ class WcMfpc
         /*
          * cache invalidation hooks
          */
-        add_action('transition_post_status', [ &$this->memcached, 'clear_ng' ], 10, 3);
+        add_action('transition_post_status', [ &$this->getMemcached(), 'clear_ng' ], 10, 3);
 
         /*
          * comments invalidation hooks
          */
         if (! empty($wcMfpcConfig->isCommentsInvalidate())) {
 
-            add_action('comment_post', [ &$this->memcached, 'clear' ], 0);
-            add_action('edit_comment', [ &$this->memcached, 'clear' ], 0);
-            add_action('trashed_comment', [ &$this->memcached, 'clear' ], 0);
-            add_action('pingback_post', [ &$this->memcached, 'clear' ], 0);
-            add_action('trackback_post', [ &$this->memcached, 'clear' ], 0);
-            add_action('wp_insert_comment', [ &$this->memcached, 'clear' ], 0);
+            add_action('comment_post', [ &$this->getMemcached(), 'clear' ], 0);
+            add_action('edit_comment', [ &$this->getMemcached(), 'clear' ], 0);
+            add_action('trashed_comment', [ &$this->getMemcached(), 'clear' ], 0);
+            add_action('pingback_post', [ &$this->getMemcached(), 'clear' ], 0);
+            add_action('trackback_post', [ &$this->getMemcached(), 'clear' ], 0);
+            add_action('wp_insert_comment', [ &$this->getMemcached(), 'clear' ], 0);
 
         }
 
         /*
          * invalidation on some other occasions as well
          */
-        add_action('switch_theme', [ &$this->memcached, 'clear' ], 0);
-        add_action('deleted_post', [ &$this->memcached, 'clear' ], 0);
-        add_action('edit_post', [ &$this->memcached, 'clear' ], 0);
+        add_action('switch_theme', [ &$this->getMemcached(), 'clear' ], 0);
+        add_action('deleted_post', [ &$this->getMemcached(), 'clear' ], 0);
+        add_action('edit_post', [ &$this->getMemcached(), 'clear' ], 0);
 
         /*
          * add filter for catching canonical redirects
@@ -120,6 +115,24 @@ class WcMfpc
     public function loadTextdomain()
     {
         load_plugin_textdomain('wc-mfpc', false, WC_MFPC_PLUGIN_DIR . 'languages/');
+    }
+
+    /**
+     * Returns the Memcached connection. Initiates it if it does not exist.
+     *
+     * @return Memcached
+     */
+    public function getMemcached()
+    {
+        if (empty($this->memcached)) {
+
+            global $wcMfpcConfig;
+
+            $this->memcached = new Memcached($wcMfpcConfig->getConfig());
+
+        }
+
+        return $this->memcached;
     }
 
 }
