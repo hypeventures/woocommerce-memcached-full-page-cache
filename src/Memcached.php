c9b44d30f24c08eebec908c0278d17dd86baaf6b
Achim Galeski <achim.galeski@gmail.com>
2019-05-05 14:02:34 +0200

    updated copyright line

--- src/Memcached.php
+++ src/Memcached.php
@@ -1,6 +1,7 @@
 <?php
 /*
-    Copyright 2019 Achim Galeski ( achim@invinciblebrands.com )
+    WooCommerce Memcached Full Page Cache - FPC the WooCommerece way via PHP-Memcached.
+    Copyright (C)  2019 Achim Galeski ( achim@invinciblebrands.com )
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 3, as
