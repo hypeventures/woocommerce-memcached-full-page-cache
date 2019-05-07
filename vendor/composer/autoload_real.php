3be4ec3e0e9828f086d8e605c016771025f8f077
Achim Galeski <achim.galeski@gmail.com>
2019-05-07 23:12:18 +0200

    autoloader now using Level 2/A https://getcomposer.org/doc/articles/autoloader-optimization.md#optimization-level-2-b-apcu-cache

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -29,22 +29,13 @@ class ComposerAutoloaderInit9042dda03b2c5b78aa2a92d82f443d22
 
             call_user_func(\Composer\Autoload\ComposerStaticInit9042dda03b2c5b78aa2a92d82f443d22::getInitializer($loader));
         } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
             $classMap = require __DIR__ . '/autoload_classmap.php';
             if ($classMap) {
                 $loader->addClassMap($classMap);
             }
         }
 
+        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
         return $loader;
