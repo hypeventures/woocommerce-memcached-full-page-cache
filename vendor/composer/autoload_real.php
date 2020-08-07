26c2bdd0c9ef2193d204a4d2fbcdeaa4d26c13f2
Achim Galeski <achim.galeski@gmail.com>
2020-08-07 11:10:56 +0200

    updated autoloader

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit9042dda03b2c5b78aa2a92d82f443d22
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
@@ -29,13 +32,22 @@ class ComposerAutoloaderInit9042dda03b2c5b78aa2a92d82f443d22
 
             call_user_func(\Composer\Autoload\ComposerStaticInit9042dda03b2c5b78aa2a92d82f443d22::getInitializer($loader));
         } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
             $classMap = require __DIR__ . '/autoload_classmap.php';
             if ($classMap) {
                 $loader->addClassMap($classMap);
             }
         }
 
-        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
         return $loader;
